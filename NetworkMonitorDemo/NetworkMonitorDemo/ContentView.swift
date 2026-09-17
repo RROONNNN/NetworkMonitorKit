4dd727fcc5c57eae7914f765b1bbedd44be60579
RROONNNN <121713445+RROONNNN@users.noreply.github.com>
2026-09-17 15:40:28 +0700

    Add NetworkMonitorKit integration and update ContentView for network status display

--- NetworkMonitorDemo/NetworkMonitorDemo/ContentView.swift
+++ NetworkMonitorDemo/NetworkMonitorDemo/ContentView.swift
@@ -1,16 +1,30 @@
-//
-
 import SwiftUI
+import NetworkMonitorKit
 
 struct ContentView: View {
+    @State  private  var status: NetworkStatus = .unknown
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Image(systemName: status.symbolName)
+                .font(.system(size: 64))
+                .foregroundStyle(status == .offline ? .red : .green)
+
+            Text(status.displayName)
+                .font(.title2)
+                .bold()
         }
         .padding()
+        .onAppear {
+            NetworkMonitor.shared.onStatusChanged {  newStatus in
+                Task {@MainActor in
+                    status = newStatus
+
+                }
+            }
+            NetworkMonitor.shared.start()
+            status = NetworkMonitor.shared.status
+        }
     }
 }
 
